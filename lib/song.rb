ae10a56889527a66238a5cf9e6dcdcb85a4f2b66
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-26 01:33:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,32 @@
-class Song
-  attr_accessor :name, :artist 
-  @@all = []
+# class Song
+#   attr_accessor :name, :artist 
+#   @@all = []
   
-  def initialize(name)
-    @name = name
-    save
-  end
+#   def initialize(name)
+#     @name = name
+#     save
+#   end
   
-def save
-  @@all << self
-end
+# def save
+#   @@all << self
+# end
 
-def artist_name=(artist_name)
-    self.artist = Artist.find_or_create_by_name(artist_name)
-  end
+# def artist_name=(artist_name)
+#     self.artist = Artist.find_or_create_by_name(artist_name)
+#   end
   
 
- def self.all
-    @@all
-end
+# def self.all
+#     @@all
+# end
 
-def self.new_by_filename(filename)
-    artist - title - genre.mp3
-    artist = filename.split(" - ")[0]
-    track = filename.split(" - ")[1]
-    song = Song.new(track)
-    song.artist_name = artist
-    song
-  end
+# def self.new_by_filename(filename)
+#     artist - title - genre.mp3
+#     artist = filename.split(" - ")[0]
+#     track = filename.split(" - ")[1]
+#     song = Song.new(track)
+#     song.artist_name = artist
+#     song
+#   end
 
-end
+# end
