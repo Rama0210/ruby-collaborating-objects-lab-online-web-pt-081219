63023ea7238d044ddbe322ea01349a4fa5e19508
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-26 01:33:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,34 @@
-class Artist 
-  attr_accessor :name, :songs  
-  @@all = []
+# class Artist 
+#   attr_accessor :name, :songs  
+#   @@all = []
   
-  def initialize(name)
-    @name = name
-    @@all << self
-    @songs = []
-  end
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#     @songs = []
+#   end
   
-  def self.all
-    @@all
-end
+#   def self.all
+#     @@all
+# end
 
-def add_song(song)
-  song.artist = self
-end
+# def add_song(song)
+#   song.artist = self
+# end
   
-  def self.find_or_create_by_name(name)
-    result = Artist.all.find do |artist|
-      artist.name == name
-    end
-  if !result
-      result = Artist.new(name)
-    end
+#   def self.find_or_create_by_name(name)
+#     result = Artist.all.find do |artist|
+#       artist.name == name
+#     end
+#   if !result
+#       result = Artist.new(name)
+#     end
 
-    result
-  end
+#     result
+#   end
 
-  def print_songs
-    puts self.songs.map { |song| song.name }
-  end
-end
+#   def print_songs
+#     puts self.songs.map { |song| song.name }
+#   end
+# end
 
